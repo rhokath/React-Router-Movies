35ccd030b45d6884f53c5e03908cf30fdfc48eb2
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-07-23 16:11:08 -0400

    mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -21,7 +22,8 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <Link to={`/movies/${movie.id}`}>
+        <MovieDetails key={movie.id} movie={movie} /></Link>
       ))}
     </div>
   );
@@ -45,6 +47,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
+      
     </div>
   );
 }
